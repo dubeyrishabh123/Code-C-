efe495276d7d0b33990f7f5553cb862460bd4c4e
Rishabh Dubey <DUBEYVISHAL690@GMAIL.COM>
2025-07-15 04:20:12 +0530

    Add files via upload

--- tree.cpp
+++ tree.cpp
@@ -4,35 +4,56 @@ class Node{
 
     public:
     int data;
-    Node* next=NULL;
-    Node* prev=NULL;
+    Node* left=NULL;
+    Node* right=NULL;
     Node(int val)
     {
          data=val;
-         next=NULL;
-         prev=NULL;
+         left=NULL;
+         right=NULL;
     }
 };
-
-Node* Binary()
+Node* form(Node* root,int target)
 {
-    int x;
-    cout<<"enter val"<<endl;
-    cin>>x;
-    if(x==-1)
-    {
-        return NULL;
-    }
-    Node* temp= new Node(x);
-    cout<<"left of "<<x<<endl;
-    temp->prev=Binary();
-    cout<<"right of"<<x<<endl;
-    temp->next=Binary ();
-    return temp;
+        if(!root)
+        {
+            Node * temp=new Node(target);
+            return temp;
+        }
+        if(target<root->data)
+        {
+            root->left=form(root->left,target);
+        }
 
+        if(target>root->data)
+        {
+            root->right=form(root->right,target);
+        }
+        return root;
+}
+void show(Node* root)
+{
+    if(!root)
+    {
+        return;
+    }
+    show(root->left);
+    cout<<root->data;
+    show(root->right);
+    
 }
 int main()
 {
-    Node* root=Binary();
+
+    int n;
+    cin>>n;
+   Node*root=NULL;
+    for(int i=0;i<n;i++)
+    {
+        int target;
+        cin>>target;
+        root=form(root,target);
+    }
+    show(root);
     return 0;
 }
